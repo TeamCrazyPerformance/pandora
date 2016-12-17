6cbdc96e81f30b627737fdde947e99ef27e76e2a
KyleSJ <sj_kyle@naver.com>
2016-12-18 00:23:01 +0900

    newDate UI

--- app/pandora/src/pages/newDate/newDate.ts
+++ app/pandora/src/pages/newDate/newDate.ts
@@ -8,23 +8,28 @@ import { LoadingController } from 'ionic-angular';
   templateUrl: 'newDate.html'
 })
 export class NewDate {
-    
-    course : {title : string, review : string, location : string, pictures : Array<string>, score : number};
+    date2={}
+    /*
+    course : {title : String, review : string, location : string, pictures : Array<string>, score : number};
+
     date : {review : string, title : string, courses : Array<Object>};
     
     constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController) {
         let date2 = this.course;
         
         date2 = {
-            title : "test",
-            review : null,
-            location : null,
-            pictures : null,
-            score : 0
+            title : '',
+            review : '',
+            location : '',
+            pictures : '',
+            score : ''
         };
+    }*/
+    newDateForm(){
+        console.log(this.date2);
     }
-    
+    /*
     backPage(){
         this.navCtrl.pop();
-    }
+    }*/
 }
