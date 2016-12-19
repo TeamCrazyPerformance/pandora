78014bcebd4e936113ab99407a4b4eb9cdd68da9
geine <bonos2@wisdo.me>
2016-12-20 05:21:15 +0900

    서버 연동 준비 완료

--- app/pandora/src/pages/newDate/newDate.ts
+++ app/pandora/src/pages/newDate/newDate.ts
@@ -4,6 +4,8 @@ import { NavController, NavParams } from 'ionic-angular';
 import { LoadingController, AlertController, ModalController } from 'ionic-angular';
 import { Camera } from 'ionic-native';
 import { MapList } from '../mapList/mapList';
+import { Http, Headers, RequestOptions } from '@angular/http';
+
 @Component({
   selector: 'page-newDate',
   templateUrl: 'newDate.html'
@@ -11,21 +13,22 @@ import { MapList } from '../mapList/mapList';
 export class NewDate {
     public base64Image : string;
     test : {count : number};
-    course : {title : string, review : string, location : string, pictures : Array<string>, score : number, price : number, locationObj : any};
-
+    course : {title : string, review : string, location : string, pictures : Array<{index : number, photo : string}>, score : number, price : number, locationObj : any, couple_id : string, seq_num : string, gps: string, contents : string, main_photo : string, photos : Array<{index : number, photo : string}>};
+    photoCount : number;
+    
     date : {review : string, title : string, courses : Array<Object>};
     count : number;
-    courses : Array<Object>;
+    courses : Array<any>;
     timer : number;
     shareMap : any;
     shareDaum : any;
     
-    constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController, public alertCtrl : AlertController, public modalCtrl: ModalController) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController, public alertCtrl : AlertController, public modalCtrl: ModalController, public http: Http) {
         
         this.test = {
             count : 0
         };
-
+        this.photoCount = 0;
         this.courses = [];
         this.count = 0;
         this.initCourse();
@@ -38,36 +41,56 @@ export class NewDate {
         this.course = {
             review: "",
             title: "",
-            location: "",
+            location: " ",
             pictures : new Array(),
             score : 0,
             price : 0,
-            locationObj : new Object()
+            locationObj : new Object(),
+            couple_id : " ", 
+            seq_num : " ", 
+            gps: " ", 
+            contents : " ", 
+            main_photo : " ", 
+            photos : new Array()
         }
     }
     newDateForm(){
         
         //마지막 카드에서 데이터 가져오는거 필요함
         //회원가입 부분 참고해
+        this.course.seq_num = this.count.toString();
+        this.course.contents = this.course.review;
+        if(this.course.pictures[0] != undefined){
+            this.course.main_photo = this.course.pictures[0].photo;    
+        }
+        this.course.couple_id = "DP32hjeEvSoj7";
+        this.course.photos = this.course.pictures;
+        this.course.locationObj = null;
         
         this.courses.push(this.course);
         this.initCourse();
-        console.dir(this.courses);
+        console.log(this.course.pictures.length);
         this.count++;
+        this.photoCount++;
     }
     backPage(){
         this.navCtrl.pop();
     }
     
-    openPicture(){
-//        Camera.getPicture({
-//            destinationType: 0,
-//            sourceType : 0
-//        }).then((imageData) => {
-//            this.base64Image = "data:image/jpeg;base64," + imageData;
-//        }, (err) => {
-//           alert(err); 
-//        });
+    openPicture(course){
+        console.log("진입");
+        Camera.getPicture({
+            destinationType: 0,
+            sourceType : 0
+        }).then((imageData) => {
+            this.course.pictures.push({
+                index : this.photoCount,
+                photo : "data:image/jpeg;base64," + imageData
+            });
+            
+        }, (err) => {
+           alert(err.json()); 
+        });
     }
     
     insertPrice(course){
@@ -224,13 +247,44 @@ export class NewDate {
             var marker = new this.shareDaum.maps.Marker({
                 position: markerPosition
             });
-
+            course.gps = markerPosition.toString();
             // 마커가 지도 위에 표시되도록 설정합니다
             marker.setMap(map);
             console.dir(marker);
             
             });
         modal.present();
+    }
+    
+    saveData(){
+        this.newDateForm();
+        
+        var headers = new Headers();
+            headers.append('Content-Type','a pplication/json');
+            var options = new RequestOptions({
+                headers: headers
+            });
+        
+        
+        
+        let data = {
+            couple_id : "DP32hjeEvSoj7",
+            days : "2016-12-23",
+            public_cond: "1",
+            course : this.courses,
+            thumbnail : ""
+        }
+        
+        if (this.courses[0].pictures[0] != undefined){
+            data.thumbnail = this.courses[0].pictures[0].photo;       
+        }
+        
+        this.http.post('https://app-pandora.azurewebsites.net/pandora/api/couples/v1.0/DP32hjeEvSoj7/dates',data,options).subscribe(
+                res => {
+                    alert("success" + res.json()); 
+                }, (err) => {
+                    alert("fail" + err.json());
+            });
     }
     
 }
